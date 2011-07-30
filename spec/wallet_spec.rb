321202bd599e1800ec25029af0ab3bd842ccdc7b
Mat Holroyd <matholroyd@gmail.com>
2011-07-30 11:46:25 +0100

    Got basic #getnewaddress and #getaddressesbyaccount working

--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/../wallet'
 
+BitCoinAddressRexExp = /^1[#{Wallet::Base58Chars}]{33}$/
+
 describe Wallet do
-  let(:wallet) { Wallet.new }
+  let(:wallet) { Wallet.new('bitcoin-wallet.cache.test').reset }
   
   context 'getbalance' do
     it 'begins as 0.0' do
@@ -11,6 +13,39 @@ describe Wallet do
   
   context 'accounts' do
     
+  end
+  
+  context 'getnewaddress' do
+    it do
+      wallet.getnewaddress.should =~ BitCoinAddressRexExp
+    end
+    
+    it do
+      a1 = wallet.getnewaddress
+      a2 = wallet.getnewaddress
+      a1.should_not == a2
+    end
+  end
+  
+  context 'getaddressesbyaccount' do
+    it do 
+      wallet.getaddressesbyaccount("").should == []
+    end
+    
+    it do
+      a = wallet.getnewaddress
+      wallet.getaddressesbyaccount("").should == [a]
+      a2 = wallet.getnewaddress
+      wallet.getaddressesbyaccount("").should == [a, a2]
+    end
+  end
+  
+  context 'listaccounts' do
+    it do
+      result = wallet.listaccounts
+      result.length.should == 1
+      result.first.should == ["", '0.00000000']
+    end
   end
   
 end
--- wallet.rb
+++ wallet.rb
@@ -1,16 +1,63 @@
 require 'pstore'
+require 'bigdecimal'
 
 class Wallet
+  Base58Chars = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a - %w{0 O I l}
+  
+  attr_reader :db_path
+  
+  def initialize(db_path = 'bitcoin-wallet.cache')
+    @db_path = db_path
+  end
+  
+  def reset
+    db.transaction do
+      db.delete(:balance)
+      db.delete(:accounts)
+      db.delete(:addresses)
+    end
+    self
+  end
+  
   def getbalance
     db.transaction do 
-      db[:balance] ||= '0.0' 
-      {'balance' => '0.0'}
+      balance = db.fetch(:balance, BigDecimal.new('0.0'))
+      {'balance' => balance.to_s}
     end
+  end
+  
+  def listaccounts
+    {'' => '0.00000000'}
+  end
+  
+  def getnewaddress 
+    result = "1" + (1..33).collect { Base58Chars[rand(Base58Chars.length)] }.join
+    t_accounts do |accounts|
+      accounts[""].addresses << result 
+    end
+    result
+  end
+  
+  def getaddressesbyaccount(account_name)
+    t_accounts[account_name].addresses
   end
   
   private
   
+  def t_accounts(&block)
+    db.transaction do 
+      accounts = db.fetch(:accounts,  {"" => Account.new("", []) })
+      yield(accounts) if block
+      db[:accounts] = accounts
+      accounts
+    end
+  end
+            
   def db
-    @db ||= PStore.new('bitcoin-wallet.cache')
+    @db ||= PStore.new(db_path)
+  end
+  
+  class Account < Struct.new(:name, :addresses)
   end
 end
+
