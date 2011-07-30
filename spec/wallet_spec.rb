38d119bae1efb764d6a3c3748c9dbbe2f693b64d
Mat Holroyd <matholroyd@gmail.com>
2011-07-30 12:35:45 +0100

    Added #test_adjust_balance

--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -1,20 +1,21 @@
 require File.dirname(__FILE__) + '/../wallet'
 
 BitCoinAddressRexExp = /^1[#{Wallet::Base58Chars}]{33}$/
+TestPath = File.dirname(__FILE__) + '/../bitcoin-wallet.cache.test'
 
 describe Wallet do
-  let(:wallet) { Wallet.new('bitcoin-wallet.cache.test').reset }
+  let(:wallet) { Wallet.new(TestPath).test_reset }
   
   context 'getbalance' do
-    it 'begins as 0.0' do
-      wallet.getbalance.should == {'balance' => '0.0'}
+    it do
+      wallet.getbalance.should == {'balance' => BigDecimal.new('0.0')}
+    end
+
+    it do
+      wallet.getbalance("some-account").should == {'balance' => BigDecimal.new('0.0')}
     end
   end
-  
-  context 'accounts' do
     
-  end
-  
   context 'getnewaddress' do
     it do
       wallet.getnewaddress.should =~ BitCoinAddressRexExp
@@ -44,8 +45,17 @@ describe Wallet do
     it do
       result = wallet.listaccounts
       result.length.should == 1
-      result.first.should == ["", '0.00000000']
+      result.first.should == ["", BigDecimal.new('0')]
     end
+  end
+  
+  describe 'interface for testing' do
+
+    it 'should adjust the balance' do
+      wallet.test_adjust_balance("", BigDecimal.new('1.5'))
+      wallet.getbalance.should == {'balance' => BigDecimal.new('1.5')}
+    end
+    
   end
   
 end
--- wallet.rb
+++ wallet.rb
@@ -10,24 +10,14 @@ class Wallet
     @db_path = db_path
   end
   
-  def reset
-    db.transaction do
-      db.delete(:balance)
-      db.delete(:accounts)
-      db.delete(:addresses)
-    end
-    self
-  end
-  
-  def getbalance
-    db.transaction do 
-      balance = db.fetch(:balance, BigDecimal.new('0.0'))
-      {'balance' => balance.to_s}
-    end
+  def getbalance(account_name = "")
+    ensure_account(account_name)
+    
+    {'balance' => t_accounts[account_name].balance}
   end
   
   def listaccounts
-    {'' => '0.00000000'}
+    {'' => BigDecimal.new('0')}
   end
   
   def getnewaddress 
@@ -42,14 +32,39 @@ class Wallet
     t_accounts[account_name].addresses
   end
   
+  # Testing methods
+  
+  def test_adjust_balance(account_name, amount)
+    t_accounts do |accounts|
+      accounts[account_name].balance = amount
+    end
+  end
+  
+  def test_reset
+    File.delete(db.path) if File.exists?(db.path)
+    self
+  end
+  
   private
   
-  def t_accounts(&block)
+  def ensure_account(account_name)
     db.transaction do 
-      accounts = db.fetch(:accounts,  {"" => Account.new("", []) })
+      accounts = db[:accounts] || {}
+      if accounts[account_name].nil?
+        accounts[account_name] = Account.new(account_name, [], BigDecimal.new('0'))
+      end
+      db[:accounts] = accounts
+    end
+  end
+  
+  def t_accounts(&block)
+    ensure_account("")
+    
+    db.transaction do 
+      accounts = db[:accounts]
       yield(accounts) if block
       db[:accounts] = accounts
-      accounts
+      db[:accounts]
     end
   end
             
@@ -57,7 +72,7 @@ class Wallet
     @db ||= PStore.new(db_path)
   end
   
-  class Account < Struct.new(:name, :addresses)
+  class Account < Struct.new(:name, :addresses, :balance)
   end
 end
 
