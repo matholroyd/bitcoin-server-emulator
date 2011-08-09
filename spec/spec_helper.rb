6157738365c19ce1a9d8a41bfceda078dc48e496
Mat Holroyd <matholroyd@gmail.com>
2011-08-09 19:53:45 +0100

    Moved test cache to be under tmp dir.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../wallet'
 
 BitCoinAddressRexExp = /^1[#{Wallet::Base58Chars}]{33}$/
-TestPath = File.dirname(__FILE__) + '/../bitcoin-wallet.cache.test'
+TestPath = File.dirname(__FILE__) + '/../tmp/bitcoin-wallet.cache.test'
 
 def bg(amount)
   BigDecimal.new(amount.to_s)
