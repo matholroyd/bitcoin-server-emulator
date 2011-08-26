20197ecf7cb95641e8917c8fb82442f49c0a6677
Mat Holroyd <matholroyd@gmail.com>
2011-08-26 15:29:10 +0200

    Can view and adjust_balance of accounts.

--- start.rb
+++ start.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
 require 'json'
+require 'erb'
 require './wallet'
+  
+set :views, File.dirname(__FILE__) + '/view'
+
+get '/:name' do |db_name|
+  @db_name = db_name
+  @accounts = wallet(db_name).listaccounts
+  
+  erb :'index.html'
+end
+
+get '/:name/adjust_balance' do |db_name|
+  wallet(db_name).helper_adjust_balance(
+    params[:account], 
+    wallet(db_name).getbalance(params[:account]).to_i + params[:amount].to_i
+  )
+  redirect "/#{db_name}"
+end
 
 post '/' do
   process_jsonrpc
